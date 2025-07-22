fa55ea54f84671d12873bd79df421a10b780fcf5
Егор <27egorau@gmail.com>
2025-07-22 20:43:58 +0300

    added validation for input GOlang

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"regexp"
 	"slices"
 
 	"net/http"
@@ -14,12 +15,29 @@ type ReverseTextResponse struct {
 	IsPalindrome bool   `json:"isPalindrome"`
 }
 
+var (
+	textRegex = regexp.MustCompile(`^[а-яА-ЯёЁa-zA-Z0-9\s]+$`)
+)
+
 func main() {
 	router := gin.Default()
-	router.GET("/", GetHandler)
-	router.POST("/", FormHandler)
+	router.GET("/", WebHandler) // Объединённый обработчик для GET
+	router.POST("/", WebHandler)
 	router.PUT("/", PutHandler)
 	router.Run("localhost:8080")
+}
+
+func validateText(text string) error {
+	if len(text) == 0 {
+		return fmt.Errorf("текст не может быть пустым")
+	}
+	if len(text) > 50 {
+		return fmt.Errorf("текст слишком длинный (максимум 50 символов)")
+	}
+	if !textRegex.MatchString(text) {
+		return fmt.Errorf("текст может содержать только буквы, цифры и пробелы")
+	}
+	return nil
 }
 
 func getStatusCode(isPalindrome bool) int {
@@ -27,6 +45,35 @@ func getStatusCode(isPalindrome bool) int {
 		return http.StatusAccepted
 	}
 	return http.StatusOK
+}
+
+func WebHandler(c *gin.Context) {
+	var text string
+	if c.Request.Method == "GET" {
+		text = c.Query("text")
+	} else {
+		text = c.PostForm("text")
+	}
+
+	if err := validateText(text); err != nil {
+		c.String(http.StatusBadRequest, err.Error())
+		return
+	}
+	reversed, isPalindrome := ReverseString(text)
+
+	// Цвет в зависимости от метода
+	var txtColor string
+	if c.Request.Method == "GET" {
+		txtColor = "green"
+	} else {
+		txtColor = "blue"
+	}
+	if isPalindrome {
+		txtColor = "red" // Палиндром всегда красный
+	}
+
+	htmlResponse := fmt.Sprintf(`<h1 style="color: %s">%s</h1>`, txtColor, reversed)
+	c.String(getStatusCode(isPalindrome), htmlResponse)
 }
 
 func GetHandler(c *gin.Context) {
